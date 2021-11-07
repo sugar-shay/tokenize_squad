61552467737eb8d77fcc3a978d24b1fb1b2c84e7
sugar-shay <72109721+sugar-shay@users.noreply.github.com>
2021-11-07 16:00:36 -0500

    Add files via upload

--- test.py
+++ test.py
@@ -350,8 +350,8 @@ def new_postprocess_qa_predictions(test_data, tokenized_test_data, raw_predictio
         valid_answers = []
         
         #context = example["context"]
-        input_ids = tokenized_test_data[example_index]
-        print('input ids keys: ', input_ids.keys())
+        input_ids = tokenized_test_data[example_index]['input_ids']
+        print('input ids shape: ', input_ids.shape)
         # Looping through all the features associated to the current example.
         for feature_index in feature_indices:
             # We grab the predictions of the model for this feature.
