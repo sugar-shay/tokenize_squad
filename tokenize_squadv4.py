bb708a85b1089ecabf235a9b62760ba65c86830f
sugar-shay <72109721+sugar-shay@users.noreply.github.com>
2021-11-18 09:30:22 -0500

    Add files via upload

--- tokenize_squadv4.py
+++ tokenize_squadv4.py
@@ -111,6 +111,107 @@ def tokenize_squad(this_example, this_tokenizer, subdataset_name=None, **kwargs
 
     return tokenized_examples
 
+
+def new_postprocess_qa_predictions(tokenized_test_data, raw_predictions, this_tokenizer, **kwargs):
+    
+    assert "squad_v2" in kwargs, "You must specify if you are using squad_v2"
+    
+    squad_v2 = kwargs['squad_v2']
+    n_best_size = kwargs['n_best_size'] if 'n_best_size' in kwargs else 20
+    
+    # The authorized overlap between two part of the context when splitting it is needed.
+    max_answer_length = kwargs['max_answer_length'] if 'max_answer_length' in kwargs else 30 
+    
+    tokenized_test_data.set_format(type=tokenized_test_data.format["type"], columns=list(tokenized_test_data.features.keys()))    
+    
+    
+    
+    all_start_logits, all_end_logits = raw_predictions
+        
+    # Build a map example to its corresponding features.
+    example_id_to_index = {k: i for i, k in enumerate(tokenized_test_data["id"])}
+    features_per_example = collections.defaultdict(list)
+    for i, feature in enumerate(tokenized_test_data):
+        features_per_example[example_id_to_index[feature["example_id"]]].append(i)
+        
+
+
+    # The dictionaries we have to fill.
+    #predictions = collections.OrderedDict()
+    predictions = []
+
+    # Let's loop over all the examples!
+    for example_index in range(len(tqdm(tokenized_test_data))):
+        # Those are the indices of the features associated to the current example.
+        feature_indices = features_per_example[example_index]
+
+        min_null_score = None # Only used if squad_v2 is True.
+        valid_answers = []
+        
+        #context = example["context"]
+        input_ids = tokenized_test_data[example_index]['input_ids']
+        #print('input ids len: ', len(input_ids))
+        # Looping through all the features associated to the current example.
+        for feature_index in feature_indices:
+            # We grab the predictions of the model for this feature.
+            start_logits = all_start_logits[feature_index]
+            end_logits = all_end_logits[feature_index]
+            # This is what will allow us to map some the positions in our logits to span of texts in the original
+            # context.
+            offset_mapping = tokenized_test_data[feature_index]["offset_mapping"]
+
+            # Update minimum null prediction.
+            cls_index = tokenized_test_data[feature_index]["input_ids"].index(this_tokenizer.cls_token_id)
+            feature_null_score = start_logits[cls_index] + end_logits[cls_index]
+            if min_null_score is None or min_null_score < feature_null_score:
+                min_null_score = feature_null_score
+
+            # Go through all possibilities for the `n_best_size` greater start and end logits.
+            start_indexes = np.argsort(start_logits)[-1 : -n_best_size - 1 : -1].tolist()
+            end_indexes = np.argsort(end_logits)[-1 : -n_best_size - 1 : -1].tolist()
+            for start_index in start_indexes:
+                for end_index in end_indexes:
+                    # Don't consider out-of-scope answers, either because the indices are out of bounds or correspond
+                    # to part of the input_ids that are not in the context.
+                    if (
+                        start_index >= len(offset_mapping)
+                        or end_index >= len(offset_mapping)
+                        or offset_mapping[start_index] is None
+                        or offset_mapping[end_index] is None
+                    ):
+                        continue
+                    # Don't consider answers with a length that is either < 0 or > max_answer_length.
+                    if end_index < start_index or end_index - start_index + 1 > max_answer_length:
+                        continue
+
+                    #start_char = offset_mapping[start_index][0]
+                    #end_char = offset_mapping[end_index][1]
+                    valid_answers.append(
+                        {
+                            "score": start_logits[start_index] + end_logits[end_index],
+                            "text": this_tokenizer.decode(input_ids[start_index: end_index+1])
+                        }
+                    )
+        
+        if len(valid_answers) > 0:
+            best_answer = sorted(valid_answers, key=lambda x: x["score"], reverse=True)[0]
+        else:
+            # In the very rare edge case we have not a single non-null prediction, we create a fake prediction to avoid
+            # failure.
+            best_answer = {"text": "", "score": 0.0}
+        
+        # Let's pick our final answer: the best one or the null answer (only for squad_v2)
+        if not squad_v2:
+            #predictions[example_index["id"]] = best_answer["text"]
+            predictions.append(best_answer["text"])
+        else:
+            answer = best_answer["text"] if best_answer["score"] > min_null_score else ""
+            #predictions[example_index["id"]] = answer
+            predictions.append(answer)
+
+    return predictions
+
+
 def main(squad_v2=False):
     
     #im trying to use less data here to see if this works
@@ -158,6 +259,13 @@ def main(squad_v2=False):
     )
     
     trainer.train()
+    
+    raw_predictions = trainer.predict(tokenized_test)
+    
+    postprocess_kwargs = {'squad_v2':False}
+    
+    final_predictions = new_postprocess_qa_predictions(tokenized_test, raw_predictions.predictions, this_tokenizer=tokenizer, **postprocess_kwargs)
+    
     
     
 if __name__ == "__main__":
